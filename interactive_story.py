38bc0f2051d98807f795481d511138f8b2beb116
Justin Jones <jus.jones@yahoo.com>
2024-07-29 21:20:45 -0500

    working on app

--- interactive_story.py
+++ interactive_story.py
@@ -3,17 +3,31 @@
 #         print(line)
     
 start_of_word = -1
-
+words = set()
 target_start = "<"
 target_end = ">"   
     
 with open("story.txt", "r") as f: 
     story = f.read()
     
-print(story[1:5])
 for i, char in enumerate(story):
     if char == target_start:
         start_of_word = 1
+    
+    if char == target_end and start_of_word != -1:
+        word = story[start_of_word: i + 1]
+        words.add(word)
+        start_of_word = -1
+
+answers = {}
+
+for word in words:
+    answers = input("Enter a word for " + word + ": ")
+    answers[word] = answers
+    
+
+for word in words:
+    story = story.replace(word, answers[word])
         
 
         
